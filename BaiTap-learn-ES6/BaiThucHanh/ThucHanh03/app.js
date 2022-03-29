da886412b4bf3e41625bb905c2d19696c18f301a
Huỳnh Ngọc Trung <mac@Macs-MacBook-Air.local>
2022-03-29 20:06:28 +0700

    Fisrt commit

---
+++ BaiTap-learn-ES6/BaiThucHanh/ThucHanh03/app.js
@@ -0,0 +1,5 @@
+import { greet, LESSON, greeting } from "./main.js";
+
+console.log(greet);
+console.log(LESSON);
+console.log(greeting("Gnurtiw", "Học cách dùng Module trong ES6"));
---
+++ BaiTap-learn-ES6/BaiThucHanh/ThucHanh03/main.js
@@ -0,0 +1,8 @@
+let greet = "Xin chào cả nhà yêu của Kem!!!";
+const LESSON = "Em muốn uống thật say để quên hết sự đời, hiểu chứ???";
+
+function greeting (name, content) {
+    return name + " " + content;
+}
+
+export { greet, LESSON, greeting };
